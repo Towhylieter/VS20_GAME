48c5be5a14a84394fe578631930da1e2248b87bc
To why lieter→ <agoppy1222@gmail.com>
2021-05-24 23:17:10 +0900

    test

---
+++ Project8/Project8/test.h
@@ -0,0 +1,23 @@
+#pragma once
+
+class test
+{
+public:
+	test();
+	~test();
+
+	// ‰¼’è‹`
+	void WTF();
+
+	void SetGlobalData();
+
+private:
+	int lol;
+};
+
+// ‚È‚ñ‚©‚Ì‰¼’è‹`
+int GetGlobalData();
+
+int ABC(){
+	return 1;
+}
